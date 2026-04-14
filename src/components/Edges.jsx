9641075412da2aa73eb7407c250e3f8b62a87789
jatin bansal <jatinhbti@gmail.com>
2026-04-15 03:49:04 +0530

    style: increase global line visibility to 0.5

--- src/components/Edges.jsx
+++ src/components/Edges.jsx
@@ -100,7 +100,7 @@ export default function Edges({ pulseRef }) {
         <lineBasicMaterial
           vertexColors={true}
           transparent={true}
-          opacity={0.2}
+          opacity={0.5}
           toneMapped={false}
           blending={THREE.AdditiveBlending}
           depthWrite={false}
